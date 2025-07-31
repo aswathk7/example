a2c6b44b2c9d88e8fe1b5419e44fc369516d534d
aswathk7 <aswathcristy@gmail.com>
2025-07-31 11:54:27 +0530

    Update SumOfTwoNumbers.java

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -1,23 +1,5 @@
-import java.util.Scanner;
-
-public class SumOfTwoNumbers {
+public class hai {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt user for first number
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        // Prompt user for second number
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-
-        // Calculate sum
-        int sum = num1 + num2;
-
-        // Display the result
-        System.out.println("The sum of the two numbers is: " + sum);
-
-        scanner.close();
+       System.out.println("HEY GUYS");
     }
 }
